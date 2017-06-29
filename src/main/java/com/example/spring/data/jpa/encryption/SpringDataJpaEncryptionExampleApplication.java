d6343e972240080622459a67138e58deeefd5d23
Damien Beaufils <damien.beaufils@gmail.com>
2017-06-29 08:55:18 +0200

    Initial import

---
+++ src/main/java/com/example/spring/data/jpa/encryption/SpringDataJpaEncryptionExampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.spring.data.jpa.encryption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaEncryptionExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaEncryptionExampleApplication.class, args);
+	}
+}
